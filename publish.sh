e47f75ef5573e39a18efebf630e6443e213d3231
qianlongxu <qianglongxu@home.mbp>
2018-04-16 01:29:55 +0800

    Theme updated:2018-04-16 01:29:54

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+localPath="../.hexo-theme-yaris"
+alias gitC='git -C $localPath'
+
+if [[ ! -d $localPath ]];then
+    mkdir $localPath
+    echo "clone to "$localPath
+    git clone git@github.com:debugly/hexo-theme-yaris.git $localPath
+else
+    echo "git pull .."
+    gitC pull origin master
+fi
+
+cp -r ./* $localPath
+
+gitC add *
+gitC commit -m "$(date +'Theme updated:%Y-%m-%d %H:%M:%S')"
+gitC push origin master
+cls
